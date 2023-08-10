6bf90935c57d85cd0d8cfcc20c9c920c3dcd01ab
Ralph Ankele <rankele@brave.com>
2023-08-10 16:11:00 +0200

    make inner product proof module public

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,7 @@ pub use crate::errors::ProofError;
 pub use crate::generators::{BulletproofGens, BulletproofGensShare, PedersenGens};
 pub use crate::linear_proof::LinearProof;
 pub use crate::range_proof::RangeProof;
+pub use crate::inner_product_proof::{inner_product, InnerProductProof};
 
 #[doc = include_str!("../docs/aggregation-api.md")]
 pub mod range_proof_mpc {
